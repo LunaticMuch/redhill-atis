bd3f292d431af7658b730724dc65bf25e69e93e5
Stefano Cislaghi <s.cislaghi@gmail.com>
2025-04-02 20:50:58 +0100

    Initial Commit

---
+++ RedhillAtis/RedhillAtisApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedhillAtisApp.swift
+//  RedhillAtis
+//
+//  Created by Stefano Cislaghi on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RedhillAtisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
