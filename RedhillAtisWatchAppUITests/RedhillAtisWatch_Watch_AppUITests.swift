dbe8fb9f1e2ee2f051d5c3fea9e4d86dbed0bd66
Stefano Cislaghi <s.cislaghi@gmail.com>
2025-04-05 15:56:22 +0100

    feat: watchos screenshots

--- RedhillAtisWatchAppUITests/RedhillAtisWatch_Watch_AppUITests.swift
+++ RedhillAtisWatchAppUITests/RedhillAtisWatch_Watch_AppUITests.swift
@@ -9,35 +9,22 @@ import XCTest
 
 final class RedhillAtisWatch_Watch_AppUITests: XCTestCase {
 
+    @MainActor
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     @MainActor
     func testExample() throws {
-        // UI tests must launch the application that they test.
+        snapshot("0Launch")
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
