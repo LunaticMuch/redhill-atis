f48353cc478922eb1c417a48be634550597bc9fe
Stefano Cislaghi <s.cislaghi@gmail.com>
2025-04-06 14:57:27 +0100

    feat: API v3

--- RedhillAtis/Models/AtisModel.swift
+++ RedhillAtis/Models/AtisModel.swift
@@ -13,48 +13,49 @@ struct RedhillAtis: Decodable {
     var designator: String
     var runway: String
     var updatedOn: String
-//    var isAutomatic: Bool
-//    var isCavok: Bool
-    var qfe: Int
-    var qnh: Int
-//    var temperature: Int
-//    var dewPoint: Int
-//    var visibility: Int
-//    var clouds: [CloudCoverage]
-//    var isWindVariable: Bool
-//    var windSpeed: Int
-//    var windBetweenFrom: Int
-//    var windDirection: Int
-//    var windBetweenTo: Int
-//    var windSpeedGust: Int
-//    var weather: String
-//
-//    struct CloudCoverage: Decodable {
-//        var type: Int = 0
-//        var height: Int = 0
-//        var cover: Int = 0
-//    }
+    var qfe: String
+    var qnh: String
+    //    var isAutomatic: Bool
+    //    var isCavok: Bool
+    //    var temperature: Int
+    //    var dewPoint: Int
+    //    var visibility: Int
+    //    var clouds: [CloudCoverage]
+    //    var isWindVariable: Bool
+    //    var windSpeed: Int
+    //    var windBetweenFrom: Int
+    //    var windDirection: Int
+    //    var windBetweenTo: Int
+    //    var windSpeedGust: Int
+    //    var weather: String
+    //
+    //    struct CloudCoverage: Decodable {
+    //        var type: Int = 0
+    //        var height: Int = 0
+    //        var cover: Int = 0
+    //    }
+    
 
     init() {
         self.site = ""
         self.metar = ""
-        self.designator = ""
-        self.runway = ""
-        self.updatedOn = ""
-//        self.isAutomatic = true
-//        self.isCavok = true
-        self.qfe = 0
-        self.qnh = 0
-//        self.temperature = 0
-//        self.dewPoint = 0
-//        self.visibility = 0
-//        self.clouds = []
-//        self.isWindVariable = false
-//        self.windSpeed = 0
-//        self.windDirection = 0
-//        self.windBetweenFrom = 0
-//        self.windBetweenTo = 0
-//        self.windSpeedGust = 0
-//        self.weather = ""
+        self.designator = "-"
+        self.runway = "00-"
+        self.updatedOn = "2025-01-01T00:00:00.000+00:00"
+        //        self.isAutomatic = true
+        //        self.isCavok = true
+        self.qfe = "0000"
+        self.qnh = "0000"
+        //        self.temperature = 0
+        //        self.dewPoint = 0
+        //        self.visibility = 0
+        //        self.clouds = []
+        //        self.isWindVariable = false
+        //        self.windSpeed = 0
+        //        self.windDirection = 0
+        //        self.windBetweenFrom = 0
+        //        self.windBetweenTo = 0
+        //        self.windSpeedGust = 0
+        //        self.weather = ""
     }
 }
---
+++ RedhillAtisWatchApp/Views/WatchInfoBox.swift
@@ -0,0 +1,18 @@
+//
+//  WatchInfoBox.swift
+//  RedhillAtisWatch Watch App
+//
+//  Created by Stefano Cislaghi on 06/04/2025.
+//
+
+import SwiftUI
+
+struct WatchInfoBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WatchInfoBox()
+}
